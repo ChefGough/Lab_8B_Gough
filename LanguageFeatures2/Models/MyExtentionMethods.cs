809e2ba35198d1cbba301f3c513c709d3ca5cedd
Alexander Gough <gougha3@my.erau.edu>
2018-09-25 09:25:41 -0400

    Lab 8B

---
+++ LanguageFeatures2/Models/MyExtentionMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures2.Models
+{
+    public static class MyExtentionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach (Product prod in productEnum)
+            {
+                total += prod.Price;
+            };
+            return total;
+        }
+        public static IEnumerable<Product> FilterByCategory(this IEnumerable<Product> productEnum, string categoryParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (prod.Category == categoryParam)
+                {
+                    yield return prod;
+                }
+            }
+        }
+        public static IEnumerable<Product> Filter(
+            this IEnumerable<Product> productEnum, Func<Product, bool> selectctorParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (selectctorParam(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+   
+}
---
+++ LanguageFeatures2/Models/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace LanguageFeatures2.Models
+{
+    public class ShoppingCart : IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
